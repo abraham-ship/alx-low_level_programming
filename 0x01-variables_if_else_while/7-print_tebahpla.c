af995eb0d2ce9a249cf3a22ecf69aa9156e57971
abraham-ship <bettabraham11@gmail.com>
2023-02-17 03:34:29 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in reverse
+ * Return: 1 if program fails. 0 otherwise.
+*/
+int main(void)
+{
+	char x;
+
+	for (x = 'z'; x <= 'a'; x--)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}
