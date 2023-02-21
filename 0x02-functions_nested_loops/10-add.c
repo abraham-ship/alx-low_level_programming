7be623ce5188f5e596819aed6e6e5ab7550a565c
abraham-ship <bettabraham11@gmail.com>
2023-02-21 03:29:35 -0800

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - add two intergers
+ * @x: first interger
+ * @y: second interger
+ * Return: return sum of the two
+ */
+int add(int x, int y)
+{
+	return (x+y);
+}
