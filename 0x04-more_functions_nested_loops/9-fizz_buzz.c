03139a16b9e7ac2f33271945f2abe38e18576fa4
abraham-ship <bettabraham11@gmail.com>
2023-02-23 03:09:50 -0800

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,6 +13,10 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
+		if (i == 100)
+		{
+			printf("Buzz");
+		}
 		if ((i % 3) == 0 && (i % 5) == 0)
 		{
 			printf("FizzBuzz ");
@@ -24,10 +28,6 @@ int main(void)
 		else if (i % 5 == 0)
 		{
 			printf("Buzz ");
-		}
-		else if (i == 100)
-		{
-			printf("Buzz");
 		}
 		else
 		{
