24da67aac6a644b296e2fd6dc42fe5c6420f7b15
abraham-ship <bettabraham11@gmail.com>
2023-03-01 10:49:43 -0800

    Always look up

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -6,16 +6,16 @@
  * @str: pointer to char.
  * Return: changed string.
  */
-char *string_toupper(char *str)
+char *string_toupper(char *sr)
 {
 	int i;
 
-	while (str[i] != '\0')
+	while (sr[i] != '\0')
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
-			str[i] = str[i] - 32;
+		if (sr[i] >= 'a' && sr[i] <= 'z')
+			sr[i] = sr[i] - 32;
 
 		i++;
 	}
-	return (str);
+	return (sr);
 }
