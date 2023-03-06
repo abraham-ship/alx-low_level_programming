061935a49f49bb31cc1a9c10f2f8fd93936cf80a
abraham-ship <bettabraham11@gmail.com>
2023-03-06 01:36:54 -0800

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _memset - a function that fills memory with a constant byte.
+ * @s: memory area pointed to by s
+ * @b: constant byte b
+ * @n: bytes to fill
+ * Return: a pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	while (n > 0)
+	{
+		*s++ = b;
+		--n;
+	}
+	return (s);
+}
