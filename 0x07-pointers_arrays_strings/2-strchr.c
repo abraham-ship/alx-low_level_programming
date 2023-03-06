0184e6fee0c4babeaba44e3bce8c1f2e1d93c8d1
abraham-ship <bettabraham11@gmail.com>
2023-03-06 01:52:22 -0800

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strchr - a function that locates a character in a string.
+ * @s: string to be searched
+ * @c: character to be searched
+ */
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++);
+	return (0);
+}
