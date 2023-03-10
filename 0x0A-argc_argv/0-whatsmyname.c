eb09f4eebd9a3cf5a8c1342c34d60ebf3d9883fe
abraham-ship <bettabraham11@gmail.com>
2023-03-10 01:29:02 -0800

    program to print its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,6 +3,7 @@
 /**
  * main - a program that prints its name, followed by a new line.
  * @argv: program name
+ * @argc: first command line argument
  * Return: 0
  */
 int main(int argc, char *argv[])
