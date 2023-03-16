7bf5c7246ab0375005d0e296d3311db9c8aa7efe
abraham-ship <bettabraham11@gmail.com>
2023-03-16 04:02:31 -0700

    allocate memory for an array

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,14 +9,17 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *ptr, i;
+	int *ptr;
+	unsigned int i;
 
-	if (nmemb == 0 || size == 0)
+	if (nmemb <= 0 || size <= 0)
 		return (NULL);
-	ptr = malloc(sizeof(nmemb) * size);
+
+	p = malloc(nmemb * size);
 	if (ptr == NULL)
 		return (NULL);
-	for (i = 0; i < size; i++)
+	for (i = 0; i < nmemb; i++)
 		ptr[i] = 0;
+
 	return (ptr);
 }
