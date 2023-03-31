2c6680ea3d0bae7e26bfd7cee4e58834e86c07a4
abraham-ship <bettabraham11@gmail.com>
2023-03-30 21:52:05 -0700

    print something before main is executed

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,6 +6,6 @@ void start_up(void) __attribute__ ((constructor));
  */
 void start_up(void)
 {
-	printf("You're beat! and yet, you must allow,"
-		"\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
 }
